5c8d6c4e06c222b0c5d20fc8611537618b4878d0
adav <Andrew#7>
2015-10-21 19:17:19 +0100

    add simple env variables

--- app.rb
+++ app.rb
@@ -5,15 +5,18 @@ set :publishable_key, ENV['PUBLISHABLE_KEY']
 set :secret_key, ENV['SECRET_KEY']
 set :server, %w[thin mongrel webrick]
 
+set :ticket_name, ENV['TICKET_NAME']
+set :ticket_price, ENV['TICKET_PRICE_PENCE'].to_i
+
 Stripe.api_key = settings.secret_key
 
 get '/' do
-  erb :checkout
+  erb :checkout, :locals => {:name => :ticket_name, :price => :ticket_price}
 end
 
 post '/pay' do
   quantity = params[:quantity].to_i > 0 ? params[:quantity].to_i : 1
-  @amount = 800 * quantity
+  @amount = :ticket_price * quantity
 
   customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
@@ -22,7 +25,7 @@ post '/pay' do
 
   charge = Stripe::Charge.create(
     :amount      => @amount,
-    :description => "#{quantity}x Earlybird JGLG Fireworks at MoHoLo Event Ticket",
+    :description => "#{quantity}x #{:ticket_name} Ticket",
     :currency    => 'gbp',
     :customer    => customer
   )
